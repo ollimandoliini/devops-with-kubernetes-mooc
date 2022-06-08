8d536c553d9dad9fdbdfdf0e3ed214c45f349a6c
Olli Kortelahti <okortelahti@gmail.com>
2022-06-08 13:41:59 +0300

    Exercise 1.05

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,10 +1,15 @@
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.get("/", (req: Request, resp: Response) => {
+  resp.type("html");
+  resp.send("<html><body>Moro</body></html>");
+});
 
 app.listen(port, () => {
   console.log(`Server started in project ${port}`);
